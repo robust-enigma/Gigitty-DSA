6a1a58d57f5fac5e89fa724b688933d3eea1d325
Shubhankit Chowdhury <74298299+robust-enigma@users.noreply.github.com>
2025-09-22 22:52:08 +0530

    Create 53 Maximum Subarray.cpp

---
+++ Arrays/53 Maximum Subarray.cpp
@@ -0,0 +1,16 @@
+//Chudane Algorithm, Sorry, Kadane's Algorith
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr_sum=nums[0];
+        int max_sum=nums[0];
+
+        for(int i=1;i<nums.size();i++)
+        {
+            curr_sum=max(nums[i],curr_sum+nums[i]);
+            max_sum=max(max_sum,curr_sum);
+        }
+
+        return max_sum;
+    }
+};
