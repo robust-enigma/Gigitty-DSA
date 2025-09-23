6bc0457204a53dc7514d807e1d619ef4630b2cbb
Shubhankit Chowdhury <74298299+robust-enigma@users.noreply.github.com>
2025-09-23 23:33:45 +0530

    Create 1 Two Sum.cpp

---
+++ Arrays/1 Two Sum.cpp
@@ -0,0 +1,19 @@
+//Akha Bollywood ek taraf, Emraan Hashmi ek taraf
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int,int>m;
+
+        for(int i=0;i<nums.size();i++)
+        {
+              if(m.find(target-nums[i])==m.end())
+              {
+                m[nums[i]] = i;
+              }
+              else
+              return {m[target-nums[i]],i};
+        }
+      return {};
+    }
+};
